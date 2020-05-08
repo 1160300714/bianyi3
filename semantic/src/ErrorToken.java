403a915c8ad4bce28b1b1885e31d8b623cfb75f7
jyri <694515124@qq.com>
2020-05-09 00:58:33 +0800

    new

---
+++ semantic/src/ErrorToken.java
@@ -0,0 +1,13 @@
+public class ErrorToken extends Token {
+	
+	public ErrorToken(String name,String source){
+		super(name,source);
+	}
+	
+	public ErrorToken(String name,String value,String source){
+		super(name,source);
+		this.value=value;
+	}
+}
+
+//token错误
